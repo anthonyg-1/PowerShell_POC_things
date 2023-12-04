4276a282068c9ac7cec910ad8fb57ede1e03ea9c
Tony Guimelli <tony.guimelli@gmail.com>
2023-12-03 23:07:58 -0500

    Update Get-CveInformation.ps1

--- Get-CveInformation.ps1
+++ Get-CveInformation.ps1
@@ -12,8 +12,8 @@ function Get-CveInformation {
     )
     BEGIN {
         # For NIST's API throttling:
-        [int]$secondsToWait = 30
-
+        [int]$secondsToWait = 60
+        ull
         [Uri]$baseUri = "https://services.nvd.nist.gov/rest/json/cves/2.0?cveId="
     }
     PROCESS {
@@ -29,18 +29,26 @@ function Get-CveInformation {
         $description = $cveData.descriptions | Where-Object lang -eq "en" | Select-Object -ExpandProperty value
 
         $metrics = $cveData.metrics
-
         $exploitabilityScore = $metrics.cvssMetricV31.exploitabilityScore
-
+        $cvssData = $metrics.cvssMetricV31.cvssData
         $impactScore = $metrics.cvssMetricV31.impactScore
 
         [PSCustomObject]@{
-            CveID               = $cveId
-            Published           = $published
-            LastModified        = $lastModified
-            Description         = $description
-            ExploitabilityScore = $exploitabilityScore
-            ImpactScore         = $impactScore
+            CveID                 = $cveId
+            Published             = $published
+            LastModified          = $lastModified
+            Description           = $description
+            BaseScore             = $cvssData.baseScore
+            BaseSeverity          = (($null -ne $cvssData.baseSeverity) ? $cvssData.baseSeverity.ToLower() : $null)
+            ExploitabilityScore   = $exploitabilityScore
+            ImpactScore           = $impactScore
+            AttackVector          = (($null -ne $cvssData.attackVector) ? $cvssData.attackVector.ToLower() : $null)
+            AttackComplexity      = (($null -ne $cvssData.attackComplexity) ? $cvssData.attackComplexity.ToLower() : $null)
+            PrivilegesRequired    = (($null -ne $cvssData.privilegesRequired) ? $cvssData.privilegesRequired.ToLower() : $null)
+            UserInteraction       = (($null -ne $cvssData.userInteraction) ? $cvssData.userInteraction.ToLower() : $null)
+            ConfidentialityImpact = (($null -ne $cvssData.confidentialityImpact) ? $cvssData.confidentialityImpact.ToLower() : $null)
+            IntegrityImpact       = (($null -ne $cvssData.integrityImpact) ? $cvssData.integrityImpact.ToLower() : $null)
+            AvailabilityImpact    = (($null -ne $cvssData.availabilityImpact) ? $cvssData.availabilityImpact.ToLower() : $null)
         }
     }
 }
